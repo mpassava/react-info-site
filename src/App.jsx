e6faff321821a9e4ab1769ea4e6b5bea164c0f2f
mpassava <mpassava@gmail.com>
2023-09-16 21:59:30 -0400

    added basic static page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
+import logo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return
+  return (
+    <div>
+      <img src={logo} width={40} />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App
